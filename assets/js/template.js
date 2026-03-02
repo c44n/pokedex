08e1ea7b63a5e1f1097a2fa053089c1e9e462299
Can <cantpts@outlook.de>
2026-03-02 03:35:14 +0100

    feat: create loadPokemons function and fetching pokemons over pokeapi, add loadPokemonDetails function and show pokemons on view

---
+++ assets/js/template.js
@@ -0,0 +1,35 @@
+
+function pokemonCardTemplate(pokemon, details, bg_color) {
+    let allTypes = "";
+
+    let types = details.types;
+
+    for (let i = 0; i < types.length; i++) {
+        let type = types[i];
+        allTypes += typesTemplate(type, bg_color);
+    }
+
+    return `
+        <div class="pokemon_card shadow bg_${bg_color}">
+            <div class="card_header">
+                <div class="pokemon_title_wrapper">
+                <h2>${pokemon.name}</h2>
+                <h3>#${details.id}</h3>
+                </div>
+                <img src="${details.sprites.other.showdown.front_default}" class="pokemon_image">
+            </div>
+
+            <div class="card_body">
+                ${allTypes}
+            </div>
+        </div>
+    `;
+}
+
+function typesTemplate(type) {
+    return `
+        <span class="type_badge">
+            ${type.type.name}
+        </span>
+    `;
+}
---
+++ script.js
@@ -0,0 +1,68 @@
+let pokemonType;
+
+const pokemonCardRef = document.getElementById('pokemon_card');
+const pokemonCardWrapperRef = document.getElementById('pokemon_card_wrapper');
+
+
+function init() {
+    loadPokemons(5);
+
+}
+
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+
+async function loadPokemons(param) {
+
+    try {
+        let response = await fetch(url);
+        // Wenn Fehler auftritt, wird restlicher Code in try nicht ausgeführt
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        let responseAsJson = await response.json();
+        let pokemons = responseAsJson.results;
+
+        //console.log(pokemons[0]);        
+
+        for (let i = 0; i < param; i++) {
+            let pokemonDetails = await loadPokemonDetails(pokemons[i]);
+            pokemonType = pokemonDetails.types[0].type.name;
+            
+            pokemonCardWrapperRef.innerHTML += pokemonCardTemplate(pokemons[i], pokemonDetails, pokemonType);
+        }
+
+
+    } catch (error) {
+        console.error('Fehler: ' + error.message);
+    }
+}
+
+async function loadPokemonDetails(pokemonData) {
+    let detailUrl = pokemonData.url;
+
+    let response = await fetch(detailUrl);
+
+    if (response.ok) {
+        let pokemonDetailsAsJson = await response.json();
+        return pokemonDetailsAsJson;
+    }
+
+}
+
+/* function getPokemonColor(typeName) {
+    let bg_color = "";    
+
+    switch (typeName) {
+        case typeName = "grass":
+            bg_color = "red";
+            break;
+        case typeName = "fire":
+            bg_color = "red";
+            break;
+        default:
+            bg_color = "white";
+            break;
+    }
+}
+ */
